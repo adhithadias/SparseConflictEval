3c7f57bfc3fd2963c0455b77c2ff1453ad1fbfcf
Adhitha Dias <kadhitha@purdue.edu>
2025-11-19 22:14:48 -0500

    add matrix scipy transpose benchmark scripts

---
+++ scripts/benchmark_transpose.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# keep matrices bcsstk17 scircuit mac_econ_fwd500 majorbasis Lin rma10 cop20k_A webbase\-1M cant pdb1HYS ecology1 largebasis consph shipsec1 atmosmodd pwtk in an array
+matrices=(
+    "bcsstk17"
+    "scircuit"
+    "mac_econ_fwd500"
+    "majorbasis"
+    "Lin"
+    "rma10"
+    "cop20k_A"
+    "webbase-1M"
+    "cant"
+    "pdb1HYS"
+    "ecology1"
+    "largebasis"
+    "consph"
+    "shipsec1"
+    "atmosmodd"
+    "pwtk"
+)
+
+outputs="Matrix, M, N, nnz, Transpose (ms)\n"
+# execute python scripts/transpose.py for each matrix and get the output
+for matrix in "${matrices[@]}"; do
+    echo "Processing matrix: $matrix"
+    output=$(python3 scripts/transpose.py -f "$matrix".mtx 2>&1)
+    wait
+    # get the last line of the output
+    last_line=$(echo "$output" | tail -n 1)
+    echo "$last_line"
+    outputs+="$last_line\n"
+done
+
+echo -e "$outputs" > data/transpose_results.csv
--- scripts/transpose.py
+++ scripts/transpose.py
@@ -1,24 +1,25 @@
+import argparse
 import time
 from scipy.io import mmread
 from scipy.sparse import csr_matrix
-import argparse
+import os
 
 BENCHMARK_FREQUENCEY = 31
 
-MATRICES_DIRECTORY = "/home/min/a/kadhitha/scratch-space/matrices"
+# get matrices directory from the environment variable TENSOR_DIR
+MATRICES_DIRECTORY = os.environ.get(
+    "TENSOR_DIR", "/home/min/a/kadhitha/scratch-space/matrices"
+)
 
-def transpose_matrix(filename : str):
-    mtx_matrix  = mmread(MATRICES_DIRECTORY + '/' + filename)
-    # mtx_matrix  = mmread('matrices/pwtk.mtx')
-    # mtx_matrix  = mmread('matrices/circuit5M.mtx')
-    csr_matrix = mtx_matrix.tocsr().astype('float32')
-    
+
+def transpose_matrix(filename: str):
+    mtx_matrix = mmread(MATRICES_DIRECTORY + "/" + filename)
+    csr_matrix = mtx_matrix.tocsr().astype("float32")
+
     print(f"filename, rows, columns, nnz, time (ms)")
     print(f"{filename}, {csr_matrix.shape[0]}, {csr_matrix.shape[1]}, ", end="")
-    # print(csr_matrix)
 
     execution_times = []
-    
     for _ in range(BENCHMARK_FREQUENCEY):
         start_time = time.time()
         transposed_matrix = csr_matrix.transpose().tocsr()
@@ -26,20 +27,17 @@ def transpose_matrix(filename : str):
         execution_times.append(end_time - start_time)
 
     # get median of execution times
-    elapsed_time = sorted(execution_times)[len(execution_times)//2]
-    
+    elapsed_time = sorted(execution_times)[len(execution_times) // 2]
     print(f"{transposed_matrix.nnz}, {1000*elapsed_time:.6f}")
-
-## add argument parser
 
 
 if __name__ == "__main__":
-    
+
     # add argument parser take filename -f as input
     argumentParser = argparse.ArgumentParser()
-    argumentParser.add_argument('-f', '--filename', help='Enter the filename of the matrix to be transposed')
+    argumentParser.add_argument(
+        "-f", "--filename", help="Enter the filename of the matrix to be transposed"
+    )
     args = argumentParser.parse_args()
-    
     filename = args.filename
-    
     transpose_matrix(filename)
