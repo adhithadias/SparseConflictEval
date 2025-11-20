286180af94a9637e5675dc8b84594003921c4d47
Adhitha Dias <kadhitha@purdue.edu>
2025-11-20 11:46:18 -0500

    add chou csr to csc conversion benchmark script

---
+++ scripts/benchmark_chou_taco_conversion.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# export NEW_TACO_COMPILER=/home/min/a/kadhitha/scratch-space/fused-compiler/taco-transpose-fused
+# export PATH=$NEW_TACO_COMPILER/buid/bin/:$PATH
+# export LD_LIBRARY_PATH=$NEW_TACO_COMPILER/build/lib:$LD_LIBRARY_PATH
+
+# TENSOR_DIR=~/scratch-space/suitesparse
+# ./build/bin/dotprod-denseout -f ~/scratch-space/suitesparse/Lin/Lin.mtx
+
+# get TENSOR_DIR from environment variable if set
+if [ -z "$TENSOR_DIR" ]; then
+    export TENSOR_DIR=~/scratch-space/suitesparse
+fi
+
+matrices=(
+    "bcsstk17"
+    "scircuit"
+    "mac_econ_fwd500"
+    "majorbasis"
+    "Lin"
+    "rma10"
+    "cop20k_A"
+    "webbase-1M"
+    "cant"
+    "pdb1HYS"
+    "ecology1"
+    "largebasis"
+    "consph"
+    "shipsec1"
+    "atmosmodd"
+    "pwtk"
+)
+
+output_file="data/chou-csr-to-csc.csv"
+outputs="Matrix, Chou Transpose (ms)\n"
+
+for matrix in "${matrices[@]}"; do
+    echo "Processing matrix: $matrix"
+    # ./build/bin/dotprod-denseout -f "$TENSOR_DIR/$matrix/$matrix.mtx"
+    output=$(./build/bin/taco-conversion "$TENSOR_DIR/$matrix/$matrix.mtx" 2)
+    wait
+    echo "$output"
+    # get the last line of the output
+    last_line=$(echo "$output" | tail -n 2)
+    # remove (ms) from the last line
+    last_line=$(echo "$last_line" | sed 's/ (ms)//g')
+    last_line="$matrix.mtx, $last_line"
+    outputs+="$last_line\n"
+    echo "$last_line"
+done
+
+echo -e "$outputs" > "$output_file"
+echo "Results saved to $output_file"
