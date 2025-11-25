1dce74b139bca703b1644682001383988efafb90
Adhitha Dias <kadhitha@purdue.edu>
2025-11-25 15:52:42 -0500

    add missing transpose benchmark to kick the tires phase

--- scripts/benchmark_kick_the_tires.sh
+++ scripts/benchmark_kick_the_tires.sh
@@ -8,7 +8,10 @@ MTX_DIR=${MTX_DIR:-/artefact/tensors/mtx}
 
 # Run tensor transpose benchmarks
 TENSOR_DIR=$MTX_DIR bash scripts/benchmark_scipy_transpose.sh
+TENSOR_DIR=$MTX_DIR bash scripts/benchmark_chou_taco_conversion.sh
 
-# Fig 11, and 12, results are saved to /artefact/SparseConflictEval/images/fig11
-# and /artefact/SparseConflictEval/images/fig12
-TENSOR_DIR=$MTX_DIR bash scripts/benchmark_elementwise-mul.sh 0
+# Fig 14, Table 1, and Table 2 stats
+TENSOR_DIR=$MTX_DIR IMAGES_DIR=/artefact/SparseConflictEval/images bash scripts/benchmark_mtx_row_stats.sh
+
+# Fig 15, results are saved to /artefact/SparseConflictEval/images/fig15
+TENSOR_DIR=$MTX_DIR bash scripts/benchmark_dotprod_denseout.sh
