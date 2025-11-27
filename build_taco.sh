e84b744257b51f2c29d7838905737b9660a96004
Adhitha Dias <kadhitha@purdue.edu>
2025-11-27 00:01:11 -0500

    update build taco script

--- build_taco.sh
+++ build_taco.sh
@@ -1,8 +1,13 @@
-!#/bin/bash
+#!/bin/bash
+
+# get TACO root from the environment variable, if not set, use default path
+if [ -z "$TACO_ROOT" ]; then
+  export TACO_ROOT=/home/min/a/kadhitha/scratch-space/taco
+fi
 
 cwd=$(pwd)
-mkdir -p /home/min/a/kadhitha/scratch-space/taco/build
-cd /home/min/a/kadhitha/scratch-space/taco/build
+mkdir -p $TACO_ROOT/build
+cd $TACO_ROOT/build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j8
 cd $cwd
